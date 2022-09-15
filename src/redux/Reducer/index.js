214ff57557eabf57b19a95e86afc93c0b753ab0a
Juan Quino <quinojuan@gmail.com>
2022-09-15 11:50:06 -0300

    first commit

---
+++ src/redux/Reducer/index.js
@@ -0,0 +1,69 @@
+const initialState = {
+    products: [],
+    details:[],
+    allProducts:[]
+}
+
+function rootReducer(state = initialState, action) {
+    switch (action.type) {
+        case 'GET_PRODUCTS':
+            return {
+                ...state,
+                products: action.payload,
+                allProducts:action.payload
+            }
+            case "GET_PHONES_BY_ID":
+                return {
+                    ...state,
+                    details: action.payload
+                }
+                case "GET_TABLETS_BY_ID":
+                    return {
+                        ...state,
+                        details:action.payload
+                    }
+                    case "GET_NOTEBOOKS_BY_ID":
+                        return{
+                            ...state,
+                            details:action.payload
+                        }
+                        case "GET_CLEAN":
+                            return{
+                                ...state,
+                                payload:[]
+                            }
+
+
+        case 'SEARCH_NAME':
+            return {
+                ...state,
+                products: action.payload
+            }
+            case "GET_FILTER_BY_CATEGORIES":
+                const productsToFilterByCategory = state.allProducts;
+                const categoryFilter = action.payload === "disabled" ?
+                productsToFilterByCategory :
+                productsToFilterByCategory?.filter(s => s.category.includes(action.payload))
+                return {
+                    ...state,
+                    products : categoryFilter
+                };
+                case "GET_FILTER_BY_RAM":
+                    const productsToFilterByRam=state.allProducts;
+                    const ramFilter=action.payload==="disabled" ?
+                    productsToFilterByRam :
+                    productsToFilterByRam?.slice(s=>s.ram.includes(action.payload))
+                    return{
+                        ...state,
+                        products:ramFilter
+                    }
+           
+        default:
+            return state;
+    }
+
+}
+
+
+
+export default rootReducer;
