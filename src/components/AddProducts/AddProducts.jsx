7fbe533f01d23b408ed611c174037feb708f030d
Juan Quino <quinojuan@gmail.com>
2022-09-28 10:11:13 -0300

    .

--- src/components/AddProducts/AddProducts.jsx
+++ src/components/AddProducts/AddProducts.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { getCart, addToCart, getPurchaseRepeat } from "../../redux/Actions";
-import style from "./AddProduct.module.css"
+// import style from "./AddProduct.module.css"
 import { Link } from "react-router-dom";
 import { useAuth } from '../Context/authContext';
 import Swap from "sweetalert2"
